7de713b542e0c3bd9648f11c93254c92e9023ef0
romanbr1 <romanbr@meta.ua>
2019-09-25 09:55:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-eu442t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-eu442t)
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import{BackgroundDirective} from './directives/background.directive';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BackgroundDirective,
+   
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
